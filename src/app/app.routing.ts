6bfc9a347ee03e5d3b13f39400b7934733ad3f74
Achref jlidi <36365666+achrefjlidi@users.noreply.github.com>
2022-07-26 15:09:07 +0100

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogDetailsComponent } from './blog/blog-details/blog-details.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+
+const routes: Routes =[
+    { path: 'home',             component: HomeComponent },
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'register',           component: SignupComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: 'login',          component: LoginComponent },
+    { path: 'products',          component: ProductsComponent },
+    { path: 'products/details',          component: ProductDetailsComponent },
+    { path: 'blog',          component: BlogComponent },
+    { path: 'blog/details', component: BlogDetailsComponent },
+    { path: 'contact',          component: ContactComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
